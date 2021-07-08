b2254546825906c33890bbdc165770e29d048a1d
Ziheng Wang <ziheng@mit.edu>
2021-07-08 18:01:46 -0400

    remove authentication

--- src/examples/aws-fun.cc
+++ src/examples/aws-fun.cc
@@ -11,19 +11,19 @@ void program_body()
 {
   ios::sync_with_stdio( false );
 
-  AWSCredentials creds {(string)"AKIAWTKT5FNZQSDCH2XD",(string)"wVW/5Kx5WvYtATwyVuydvYVIEGs+ZO4cRr+hkRvZ"};
+  AWSCredentials creds {(string)key1,(string)keyw};
     EventLoop event_loop;
-//  AWSLambdaClient aws { "us-west-2" ,creds};
+   AWSLambdaClient aws { "us-west-2" ,creds};
 //
 //
-//  aws.install_rules( event_loop );
+  aws.install_rules( event_loop );
 //  aws.print_response();
 // //aws.get_account_settings();
-//  aws.invoke_function("test");
+  aws.invoke_function("test");
 
-    AWSS3Client aws { "us-west-2" ,creds};
-    aws.install_rules( event_loop );
-    aws.download_file("dpc-h-1gb-marsupialtail","customer/customer.tbl","customer.csv");
+//    AWSS3Client aws { "us-west-2" ,creds};
+//    aws.install_rules( event_loop );
+//    aws.download_file("dpc-h-1gb-marsupialtail","/customer/customer.tbl","customer.csv");
 
     while ( event_loop.wait_next_event( -1 ) != EventLoop::Result::Exit ) {
   }
